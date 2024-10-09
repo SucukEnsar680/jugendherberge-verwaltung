d2fbfbdf01330adbf9d23294e2b082c29cb8e091
bilal-ensar.bugday@student.htl-rankweil.at <bilal-ensar.bugday@student.htl-rankweil.at>
2024-10-09 10:04:29 +0000

    Created Startseite and Database_Server; Edited settings; Deleted Form1

---
+++ client_code/Startseite/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import StartseiteTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+
+class Startseite(StartseiteTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    self.drop_down_1.items = [("Feldkirch", 0), ("Mordor", 1)]
+    print(anvil.server.call("say_hello", "sauron"))
+    print(anvil.server.call("get_jugendherbergen"))
